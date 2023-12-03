8aa3c2642deba1410af0a8641fce5c85d305d066
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-12-03 11:44:49 +0100

    update

--- modules/libUpdate.go
+++ modules/libUpdate.go
@@ -5,8 +5,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"runtime"
 )
 
@@ -51,35 +49,11 @@ func Upd_main() {
 		panic(err)
 	}
 
-	if runtime.GOOS == "windows" {
-		fmt.Println("Creating update script")
-		batch := []byte(fmt.Sprintf(`@echo off
-:wait
-tasklist /fi "imagename eq %s" 2>nul | find /i /n "%s">nul
-if "%errorlevel%"=="0" goto wait
-move /Y "%s" "%s"
-del "%s"
-`, filepath.Base(exePath), filepath.Base(exePath), outputFile, exePath, oldExePath))
-
-		script := filepath.Join(filepath.Dir(exePath), "bu_update.bat")
-		err = os.WriteFile(script, batch, 0644)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Running update script")
-		cmd := exec.Command("cmd.exe", "/C", "start", "/B", script)
-		err = cmd.Start()
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		fmt.Println("Replacing the current version with the new version")
-		err = os.Rename(outputFile, exePath)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Update completed successfully!")
+	fmt.Println("Replacing the current version with the new version")
+	err = os.Rename(outputFile, exePath)
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Println("Update completed successfully!")
 }
--- update.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:wait
-tasklist /fi "imagename eq boofutils.exe" 2>nul | find /i /n "boofutils.exe">nul
-if "%!e(string=boofutils_new.exe)rrorlevel%!"(string=B:\git\Boofutils\boofutils.exe)=="0" goto wait
-move /Y "B:\git\Boofutils\boofutils.exe_old" "%!s(MISSING)"
-del "%!s(MISSING)"
