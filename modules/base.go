bcca793f490f27da6d3bb6649e702ff5bf95e7e7
Boofdev <hexahigh0@gmail.com>
2023-12-05 12:24:57 +0100

    update

--- modules/base.go
+++ modules/base.go
@@ -6,11 +6,24 @@ import (
 	"encoding/hex"
 )
 
-func B64_string(input string, eod string) string {
-	if eod == "e" || eod == "" {
+func Base_main(base int, eod bool, stringORbin bool, input string) {
+	if stringORbin == true {
+		switch base {
+		case 16:
+			B16_string(input, eod)
+		case 32:
+			B32_string(input, eod)
+		case 64:
+			B64_string(input, eod)
+		}
+	}
+}
+
+func B64_string(input string, eod bool) string {
+	if eod == true {
 		return base64.StdEncoding.EncodeToString([]byte(input))
 	}
-	if eod == "d" {
+	if eod == false {
 		decoded, err := base64.StdEncoding.DecodeString(input)
 		if err != nil {
 			return input
@@ -20,11 +33,11 @@ func B64_string(input string, eod string) string {
 	return "An error occured while encoding/decoding"
 }
 
-func B64_data(input []byte, eod string) string {
-	if eod == "e" || eod == "" {
+func B64_data(input []byte, eod bool) string {
+	if eod == true {
 		return base64.StdEncoding.EncodeToString(input)
 	}
-	if eod == "d" {
+	if eod == false {
 		decoded, err := base64.StdEncoding.DecodeString(string(input))
 		if err != nil {
 			return string(input)
@@ -34,11 +47,11 @@ func B64_data(input []byte, eod string) string {
 	return "An error occured while encoding/decoding"
 }
 
-func B32_string(input string, eod string) string {
-	if eod == "e" || eod == "" {
+func B32_string(input string, eod bool) string {
+	if eod == true {
 		return base32.StdEncoding.EncodeToString([]byte(input))
 	}
-	if eod == "d" {
+	if eod == false {
 		decoded, err := base32.StdEncoding.DecodeString(input)
 		if err != nil {
 			return input
@@ -48,11 +61,11 @@ func B32_string(input string, eod string) string {
 	return "An error occured while encoding/decoding"
 }
 
-func B32_data(input []byte, eod string) string {
-	if eod == "e" || eod == "" {
+func B32_data(input []byte, eod bool) string {
+	if eod == true {
 		return base32.StdEncoding.EncodeToString(input)
 	}
-	if eod == "d" {
+	if eod == false {
 		decoded, err := base32.StdEncoding.DecodeString(string(input))
 		if err != nil {
 			return string(input)
@@ -62,11 +75,11 @@ func B32_data(input []byte, eod string) string {
 	return "An error occured while encoding/decoding"
 }
 
-func B16_string(input string, eod string) string {
-	if eod == "e" || eod == "" {
+func B16_string(input string, eod bool) string {
+	if eod == true {
 		return hex.EncodeToString([]byte(input))
 	}
-	if eod == "d" {
+	if eod == false {
 		decoded, err := hex.DecodeString(input)
 		if err != nil {
 			return input
@@ -76,11 +89,11 @@ func B16_string(input string, eod string) string {
 	return "An error occured while encoding/decoding"
 }
 
-func B16_data(input []byte, eod string) string {
-	if eod == "e" || eod == "" {
+func B16_data(input []byte, eod bool) string {
+	if eod == true {
 		return hex.EncodeToString(input)
 	}
-	if eod == "d" {
+	if eod == false {
 		decoded, err := hex.DecodeString(string(input))
 		if err != nil {
 			return string(input)
