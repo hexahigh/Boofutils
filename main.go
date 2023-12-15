e424950c90c6f4172e1a2f39267c3bcf6cfbff2f
Boofdev <hexahigh0@gmail.com>
2023-12-15 11:54:54 +0100

    updated version number

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 //go:embed LICENSE
 var LICENSE embed.FS
 
-const AppVersion = "1.4.0"
+const AppVersion = "1.4.2"
 
 var subD_threads int
 var skipTo, subD_domain, FIA_in, FIA_out, bua_in, bua_out, ansiimg_filename, ansiimg_output string
@@ -181,7 +181,7 @@ func main() {
 	}
 
 	if *version {
-		fmt.Println("\033[36mBoofutils\033[0m")
+		fmt.Println(m.ColorCyanBold24bit, "Boofutils", m.ColorReset)
 		fmt.Println("Version:", AppVersion)
 		fmt.Println("2023 - Boofdev")
 		fmt.Println("github.com/hexahigh/boofutils")
