f7039b111d9460bd68d83799f125ed27917eee7f
Boofdev <hexahigh0@gmail.com>
2024-01-28 17:48:22 +0100

    fix report

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 //go:embed LICENSE
 var LICENSE embed.FS
 
-const AppVersion = "1.5.3"
+const AppVersion = "1.5.4"
 
 var subD_threads int
 var skipTo, subD_domain, FIA_in, FIA_out, bua_in, bua_out, ansiimg_filename, ansiimg_output string
@@ -108,7 +108,7 @@ func init() {
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "report":
-			out := reportCommand.String("o", "", "Output file")
+			out := reportCommand.String("o", "report.json", "Output file")
 			stdout := reportCommand.Bool("s", false, "Print to stdout")
 			reportCommand.Parse(os.Args[2:])
 			m.Report(*out, *stdout)
